456bd6e1cabf48c47cf14e60849da5e99c8decc4
fariznau <fariznau21@yahoo.com>
2018-02-18 08:08:09 -0800

    M.FARIZNAUFAL_1202154149_MODUL2

---
+++ app/src/main/java/android/example/com/mfariznaufal_1202154149_modul2/MenuUtama.java
@@ -0,0 +1,42 @@
+package android.example.com.mfariznaufal_1202154149_modul2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+
+public class MenuUtama extends AppCompatActivity {
+    private Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_utama);
+        Toast.makeText(this, "Muhammad Fariz Naufal_1202154149", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onButtonClicked(View view) {
+
+        RadioButton DineIn = (RadioButton) findViewById(R.id.dinein);
+        RadioButton TakeAway = (RadioButton) findViewById(R.id.takeaway);
+
+        if(DineIn.isChecked()){
+            dineIn();
+        }
+        else if (TakeAway.isChecked()){
+            takeAway();
+        }
+
+    }
+    public void dineIn(){
+        intent = new Intent(this, DineIn.class);
+        startActivity(intent);
+    }
+    public void takeAway(){
+        intent = new Intent(this, TakeAway.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/test/java/android/example/com/mfariznaufal_1202154149_modul2/DaftarMenuTest.java
@@ -0,0 +1,10 @@
+package android.example.com.mfariznaufal_1202154149_modul2;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by USER on 2/18/2018.
+ */
+public class DaftarMenuTest {
+
+}
